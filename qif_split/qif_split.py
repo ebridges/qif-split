4394c957c39c16bdbcd50ea48d44be2396c5a5ec
Edward Bridges <github@eqbridges.com>
2017-12-01 21:14:17 -0500

    add initial logic to load split configuration

--- qif_split/qif_split.py
+++ qif_split/qif_split.py
@@ -1,29 +1,44 @@
 '''
-Naval Fate.
+QIF Split
 
 Usage:
-  naval_fate [--verbose]
-  naval_fate -h | --help
-  naval_fate --version
+  qif-split split --qif-input=<PATH> [--split-cfg=<PATH>] [--verbose]
+  qif-split -h | --help
+  qif-split --version
 
 Options:
-  -h --help     Show this screen.
-  --version     Show version.
-  --verbose     Debug-level output.
+  --qif-input=<PATH>   Input QIF file.
+  --split-cfg=<PATH>   Input configuration of splits [Default: ./split-config.json]
+  -h --help            Show this screen.
+  --version            Show version.
+  --verbose            Debug-level output.
 '''
 
 
 from .version import __version__
 from .util import configure_logging
-from logging import info, debug
+from logging import info, debug, error
 from docopt import docopt
+import json
+from jsoncomment import JsonComment
+
+def process_splits(config, qif_file):
 
 
-def do_something():
-  pass
-
+def load_split_config(config):
+  parser = JsonComment(json)
+  with open(config) as fp:
+    return parser.load(fp)
 
 def main():
   args = docopt(__doc__, version=__version__)
   configure_logging(args['--verbose'])
-  do_something()
+
+  split_config = load_split_config(args['--split-cfg'])
+
+  qif_file = args['--qif-input']
+
+  if qif_file:
+    process_splits(split_config, qif_file)
+  else:
+    error('qif-input not specified.')
