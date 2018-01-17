551f83a3d38e0712c4bab304dfb70eccb1d8d3f3
Edward Bridges <github@eqbridges.com>
2018-01-16 20:38:27 -0500

    Revert "install deps with setup.py"
    This reverts commit b2858efb1edc0293a74e355c7eac75829ff7706d.

--- setup.py
+++ setup.py
@@ -4,9 +4,6 @@ from setuptools import setup, find_packages
 app_name='qif-split'
 app_dir='qif_split'
 
-with open('requirements.txt') as f:
-  REQUIRED = [line.rstrip('\n') for line in f]
-
 version_string = None
 with open('%s/version.py' % app_dir) as f:
   for line in f:
@@ -17,7 +14,6 @@ setup(
     name = app_name,
     version = version_string,
     packages=find_packages(),
-    install_requires=REQUIRED,
     include_package_data=True,
     long_description=__doc__,
     entry_points={
